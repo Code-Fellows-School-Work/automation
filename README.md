ee77c1b19e40b3b69b91cb54d4f48312a92e815c
evidad <errolvidad@gmail.com>
2024-02-01 22:01:43 -0800

    updated readme

---
+++ README.md
@@ -0,0 +1,39 @@
+# LAB - Class 19
+
+## Project: Automation
+
+### Author: Errol Vidad
+V.1.0.0 (Pr: https://github.com/Code-Fellows-School-Work/automation/pull/1)
+
+### Links and Resources
+- Back-end server url (when applicable): None
+- Front-end application (when applicable): None
+
+### Setup
+- .venv requirements (where applicable)
+- run pip install -r requirements.txt
+
+i.e.
+
+- PORT - Port Number: None
+- DATABASE_URL - URL to the running Postgres instance/db: None
+
+### How to initialize/run your application (where applicable)
+
+- Run python -m automation.dev_lab
+
+### How to use your library (where applicable)
+### Tests
+How do you run tests?
+
+N/A
+
+### TODO
+- Finish the remaining feature requirements:
+    - Move log files into a log folder 
+    - Create a log folder if it doesn't already exist
+        - Parse for log file type for errors and warnings file names
+        - Move errors and warning file names into errors and warnings folders
+    - Move email files into an email folder
+    - Create an email folder if it doesn't already exist
+    - Count number of files in directory
--- automation/dev_lab.py
+++ automation/dev_lab.py
@@ -31,61 +31,16 @@ def remove_user(user_name):
         
         # move the directory to the recycle bin
         shutil.move(source_path, target_path)
-        
+
     except FileNotFoundError:
         console.print("[bold red] Directory or file not found [/bold red]")
-
-def list_files(directory):
-    """
-    Display all the files in the directory
-    """
-    try:
-        files = os.listdir(directory)
-        table = Table(title=f"Files in {directory}")
-        table.add_column("File Name", style="dim")
-        for file in files:
-            table.add_row(file)
-        console.print(table)
-    except FileNotFoundError:
-        console.print("[bold red] File not found [/bold red]")
-
-def move_file(directory, file, target_directory):
-    """
-    Move the given file from the current directory to the target directory
-    """
-    try:
-        source_path = os.path.join(directory, file) # platform specific full file path
-        target_path = os.path.join(target_directory, file)
-        shutil.move(source_path, target_path)
-        console.print(f"{file} moved from {directory} to {target_directory}")
-    except FileNotFoundError:
-        console.print("[bold red] Directory or file not found [/bold red]")
-
-def search_files(directory, pattern):
-    """
-    Searches for a file in a directory
-    """
-    try:
-        # files refers to all the file names in the directory
-        files = os.listdir(directory)
-        matches = [file for file in files if re.search(pattern, file)]
-        table = Table(title=f"Files in {directory}")
-        table.add_column("Matching File Name", style="dim")
-        for match in matches:
-            table.add_row(match)
-        console.print(table)
-    except FileNotFoundError:
-        console.print("[bold red] Directory not found [/bold red]")
-
-def test(args):
-    print(args)
 
 
 if __name__ == "__main__":
 
     while True:
     # console.print adds the formatting to the numbers
-        console.print("\n1. Create new directory\n2. Delete user\n3. Search Files\n4. Exit")
+        console.print("\n1. Create new directory\n2. Delete user\n3. coming soon\n4. Exit")
         choice = Prompt.ask("Choose a task (Enter the number)")
 
         if choice == "1":
@@ -97,5 +52,5 @@ if __name__ == "__main__":
         # elif choice == "3":
         #     directory = Prompt.ask("Enter the directory to search files")
         #     pattern = Prompt.ask("Enter the regex pattern to search for:")
-        # else:
+        else:
             break
